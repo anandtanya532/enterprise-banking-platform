09ba46aae10813f85f15222d3f87301ad18dab62
Anand <tanya.anand@dbschenker.com>
2026-08-11 15:59:31 +0530

    Make database seed idempotent

--- application/account-service/seed.py
+++ application/account-service/seed.py
@@ -3,18 +3,26 @@ from models import Customer
 
 db = SessionLocal()
 
-customer = Customer(
-    customer_name="Tanya Anand",
-    account_number="123456789",
-    account_type="Savings",
-    balance=150000,
-    currency="INR"
-)
+existing_customer = db.query(Customer).filter(
+    Customer.account_number == "123456789"
+).first()
 
-db.add(customer)
+if existing_customer:
+    print("Customer already exists. Skipping seed.")
+else:
+    customer = Customer(
+        customer_name="Tanya Anand",
+        account_number="123456789",
+        account_type="Savings",
+        balance=150000,
+        currency="INR"
+    )
 
-db.commit()
+    db.add(customer)
+    db.commit()
+
+    print("Customer inserted.")
 
 db.close()
 
-print("Customer inserted.")
+
